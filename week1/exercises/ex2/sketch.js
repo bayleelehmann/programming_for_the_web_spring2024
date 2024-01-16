9cf3a4da629d6a3a030e20d7a7b6e1792f47e29b
baylee lehmann <baylee.lehmann@gmail.com>
2024-01-15 20:10:37 -0800

    Exercise 2
    Challenge 2

---
+++ week1/exercises/ex2/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+createCanvas (480, 120);
+}
+
+function draw() {
+  background(204);
+  ellipse(75, 60, 90, 90);
+  strokeweight(8);
+  ellipse(175, 60, 90, 90);
+  ellipse(279, 60, 90, 90);
+  strokeweight(20);
+  ellipse(389, 60, 90, 90);
+}
