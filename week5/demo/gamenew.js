7246d2bbe76d7f36c486b415695fc32387257176
Baylee Lehmann <baylee.lehmann@gmail.com>
2024-02-14 06:49:46 -0800

    Update gamenew.js
    demo practice

--- week5/demo/gamenew.js
+++ week5/demo/gamenew.js
@@ -8,13 +8,36 @@ let cards = [];
 const gameState = {
 
 };
+let cardFaceArray = [];
+let cardBack;
+function preload() {
+    cardBack = loadImage('image/cardback.png');
+    cardFaceArray = [
+        loadImage('images/lizard.png'),
+        loadImage('images/rat.png'),
+        loadImage('images/scorpion.png'),
+        loadImage('images/snake.png'),
+        loadImage('images/wolf.png'),
+    ]
+}
 //making the card class
 function setup() {
     createCanvas(800, 600);
     background(0);
+    let selectedFaces = [];
+    for (let z = 0; z < 5; z++) {
+        const randomIdx = floor(random(cardFaceArray.length));
+        const face = cardFaceArray[randomIdx];
+        selectedFaces.push(face);
+        selectedFaces.push(face);
+        //remove the used cardface so it doesn't get randomly selected again
+        cardFaceArray.splice(randomIdx, 1);
+    }
+    selectedFaces = shuffleArray(selectedFaces);
     for (let j = 0; j < 2; j++) {
         for (let i = 0; i < 5; i++) {
-            cards.push(new Card(startingX, startingY));
+            const faceImage = selectedFaces.pop();
+            cards.push(new Card(startingX, startingY, faceImage[0]));  //cardFaceArray
             startingX += 120;
             //console.log(startingX);
     }
@@ -28,14 +51,14 @@ function setup() {
 function mousePressed() {
     for (let k = 0; k , cards.length; k++) {
         if(cards[k].didHit(mouseX, mouseY)) {
-            console.log('flipped');
+            console.log('flipped', cards[k]);
         }
     }
     //console.log(myCard.didHit(mouseX, mouseY));
 }
 
 class Card {
-    constructor (x, y) {//property
+    constructor (x, y, cardFaceImg) {//property
         //this.x = 100;
         //this.y = 100;
         this.x = x;
@@ -43,15 +66,18 @@ class Card {
         this.width = 80;
         this.height = 100;
         this.face = DOWN;
+        this.cardFaceImg = cardFaceImg;
         this.show();
     }
     show () { //methods
        if(this.face === DOWN) {
         fill('rgb(57.9%, 9.9%, 9.9%');
         rect(this.x, this.y, this.width, this.height, 10);
+        image(cardBack, this.x + 10, this.y + 20);
        } else {
         fill('#aaa');
         rect(this.x, this.y, this.width, this.height, 10);
+        image(this.cardFaceImg, this.x + 4, this.y + 15);
        }
            
     }
@@ -73,9 +99,20 @@ class Card {
             this.face = DOWN;
         } 
         this.show();
+    }   
+}
+function shuffleArray (array) {
+    let counter = array.length;
+    while (counter . 0) {
+        //pick random index
+        const idx = Math.floor(Math.random() * counter);
+        //decrease counter by 1 (decrement)
+        counter--;
+        // swap the last element with it
+        const temp = array[counter];
+        array[counter] = array[idx];
+        array[idx] = temp;
     }
+    return array;
 }
 
-//setting up the flip
-
-//making the grid and images
