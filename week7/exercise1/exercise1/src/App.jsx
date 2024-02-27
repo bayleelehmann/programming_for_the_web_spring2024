467e24698f69f5dddf3ee14b3abe1222fae6d4db
Baylee Lehmann <baylee.lehmann@gmail.com>
2024-02-27 06:53:37 -0800

    exercise1

--- week7/exercise1/exercise1/src/App.jsx
+++ week7/exercise1/exercise1/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Camelid from "./components/Camelid";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const camelData = [
+    {name: "Llama", image: "image/llama.jpeg", desc: "Llama"},
+    {name: "Alpaca", image: "image/alpaca.jpeg", desc: "Alpaca"},
+    
+  ]
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Llama and Alpaca</h2>
+      <Camelid name={camelData[0].name} image={camelData[0].image} desc={camelData[0].desc} />
+      <Camelid name={camelData[1].name} image={camelData[1].image} desc={camelData[1].desc} />
+    
+
+    </div>
   )
 }
-
-export default App
+ export default App;
---
+++ week7/exercise1/exercise1/src/camel/Camelid.jsx
@@ -0,0 +1,16 @@
+import "./camel.css"
+function Camelid({name, image, desc}) {
+    console.log(name)
+    return (
+        <div className= "camel">
+            <h1>{name}</h1>
+            <p>{image}</p>
+            <div style={{
+                width: "25px",
+                height: "25px",
+            }}></div>
+        </div>
+    );
+}
+
+export default Camelid;
