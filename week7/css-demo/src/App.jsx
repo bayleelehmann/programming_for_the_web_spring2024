6cd0a1ecb1e0a128f6cb032bc20a296fea2549d4
Baylee Lehmann <baylee.lehmann@gmail.com>
2024-02-28 06:46:43 -0800

    Update App.jsx

--- week7/css-demo/src/App.jsx
+++ week7/css-demo/src/App.jsx
@@ -1,10 +1,21 @@
 import "./styles.css";
 function App() {
-  const isKenOld = "yes";
-
+  const kenAge = "24";
+  const paragraphStyleObj = {
+    color: "green" , 
+    fontsize: "36px",
+    margin: "20px auto",
+    width: "600px"
+,  }
   return (
-    //<h1 className= "blue">Css Demo</h1>
-    <h2 className= {isKenOld === "yes" ? "blue" : "red"}>Css Demo</h2>
+    <>
+    <h1 className= {kenAge > 18 && "blue"}>Css Demo</h1>
+    <h2 className= {paragraphStyleObj === "yes" ? "blue" : "red"}>Css Demo</h2>
+    <p style = {{
+      color: "green", 
+      fontsize: "36px"
+      }}>This is some random text</p>
+    </>
   )
 }
 
