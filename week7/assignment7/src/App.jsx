582bed1ffa2d6521dd411df3be252795352289be
Baylee Lehmann <baylee.lehmann@gmail.com>
2024-03-02 06:20:55 -0800

    rewatching demos

---
+++ week7/assignment7/src/App.jsx
@@ -0,0 +1,19 @@
+// import "./styles.css";
+// function App() {
+//   const listOther = "no";
+//   const paragraphStyleObj = {
+//     color: "green",
+//     fontSize: "36px",
+//     margin: "15px auto",
+//     width: "600px",
+//   }
+//   return (
+//     <>
+//      <h1 className={listOther === "yes" ? "blue" : "red"}>CSS Demo</h1>
+//      <p style={paragraphStyleObj}>Some random text</p>
+//     </>
+   
+//   )
+// }
+
+// export default App
