906554eda5158f919585f30d9b594e7923ac81ea
Baylee Lehmann <baylee.lehmann@gmail.com>
2024-03-02 08:31:42 -0800

    Update App.jsx

--- week7/assignment7/src/App.jsx
+++ week7/assignment7/src/App.jsx
@@ -20,31 +20,58 @@
 
 import "./styles.css";
 function App() {
-  const superHeros = [
-    {name: "Superman", colors: ["blue", "red"]},
-    {name: "Batman", colors: ["black", "yellow"]},
-    {name: "Spiderman", colors: ["red", "blue"]},
-    {name: "Wonder Woman", colors: ["red", "blue", "gold"]},
+  const tabiShoes = [
+    {name: "Tabi Knee-High Boots", 
+    desc: "Crafted in vintage soft leather, these block-heel knee-high boots feature the Tabi split-toe, inspired by the traditional 15th century Japanese sock bearing the same name – launched for the Maison's debut collection in 1989. 8cm heel" ,
+    brand: "Maison Margiela", 
+    color: "Black"},
+
+    {name: "Tabi ballerinas",
+     desc: "The Tabi ballerina shoes are crafted from soft nappa leather with a 3cm cylindrical heel. They feature the Tabi split-toe, inspired by the traditional 15th century Japanese sock bearing the same name – launched for the Maison's debut collection in 1989.",
+     brand: "Maison Margiela", 
+     color: "Pale Blue"},
+
+     {name: "Tabi Mary-Janes",
+     desc: "In calf leather with an adjustable top strap, the Mary-Jane shoes feature the Tabi split-toe, inspired by the traditional 15th century Japanese sock bearing the same name – launched for the Maison's debut collection in 1989.", 
+     brand: "Maison Margiela", 
+     color: "Black"},
+
+     {name: "Tabi loafers",
+     desc: "Crafted from brushed calfskin, the chunky cleated sole loafers feature the Tabi split-toe, inspired by the traditional 15th century Japanese sock bearing the same name – launched for the Maison's debut collection in 1989.", 
+     brand: "Maison Margiela", 
+     color: "Fir"},
+
+     {name: "Tabi pumps",
+     desc: "In soft nappa leather with an adjustable ankle strap, the 8cm cylindrical heel pumps feature the Tabi split-toe, inspired by the traditional 15th century Japanese sock bearing the same name – launched for the Maison's debut collection in 1989.", 
+     brand: "Maison Margiela", 
+     color: "Dark Brown"},
+     
+     {name: "Tabi sandals",
+     desc: "With a double adjustable strap design, crafted in calf leather, the sandals feature the Tabi split-toe, inspired by the traditional 15th century Japanese sock bearing the same name – launched for the Maison's debut collection in 1989.", 
+     brand: "Maison Margiela", 
+     color: "Black"},
+    
   ];
 
   return (
     <div>
-      <h1>Rendering Lists</h1>
-      {
-        superHeros.map((hero) => {
-          return (
-            <div key={hero.name}> 
-              <h3>{hero.name} </h3>
-              <ul>
-                {hero.colors.map((color) => {
-                  return (
-                  <li key={color} style={{backgroundColor: color}} />)
-                })}
-              </ul>
+      <h1>Maison Margiela Tabi Shoes I wish I owned</h1>
+      { 
+      tabiShoes.map((tabi) => {
+        return(
+          <div key={tabi.name}>
+            <h3>{tabi.name}</h3>
+            <ul>
+              {/* {tabi.desc.map((desc) => {
+                return (
+                  <li key={desc} />)
+              })} */}
+             
+            </ul>
+
             </div>
-          )
-        })
-      }
+        )
+      })}
     </div>
   )
 }
