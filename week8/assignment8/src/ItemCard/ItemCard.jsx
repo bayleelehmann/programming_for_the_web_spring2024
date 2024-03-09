5b9c25f5ac6fd5d2acaeaf4ee6c5c5e6e572e5f7
Baylee Lehmann <baylee.lehmann@gmail.com>
2024-03-09 11:08:59 -0800

    updates

--- week8/assignment8/src/ItemCard/ItemCard.jsx
+++ week8/assignment8/src/ItemCard/ItemCard.jsx
@@ -1,15 +1,15 @@
 import PropTypes from "prop-types";
 import "./ItemCard.css";
 import clsx from "clsx";
-import label from "../assets/Maison-Margiela-Logo.png";
-import trash from "../assets/trash_icon.svg";
-import copy from "../assets/copy_icon.svg";
+// import label from "../assets/Maison-Margiela-Logo.png";
+import trash from "../assets/trash_icon.jpg";
+import copy from "../assets/copy_icon.jpg";
 //import TabiShoes from "../TabiShoes/TabiShoes";
 export default function ItemCard({
     name, 
     image, 
     // desc, 
-    brand,
+    // brand,
     color,
     id,
     deleteFn,
@@ -17,12 +17,14 @@ export default function ItemCard({
 }) {
     return (
         <div className="tabiCard">
-        <div className="label"> <img src={label} title="brand label" /> {brand}</div>
+            {/* {<div className="labelLogo">
+            <img src={label} alt="brand name"/>{label}</div>} */}
+        {/* <div className="label"> <img src={label} title="brand label" /> {brand}</div> */}
            <div className="cardImg">
            <img src={image} alt={name} />
         </div>
             <div className="cardTitle">{name}</div>
-            {/* <div className="tabiDesc">{desc}</div> */}
+            {/* {<div className="tabiDesc">{desc}</div>} */}
             <div className="actions">
                 <a href="#" onClick={(evt) => {
                     evt.preventDefault();
@@ -33,9 +35,10 @@ export default function ItemCard({
                     duplicateFn(id)
                 }}><img src={copy} /></a>
             </div>
-            {color.map((color) => {
+            <div className={clsx([color])}>{color}</div>
+            {/* {color.map((color) => {
                 return <div key={color} className={clsx(["stripe", color])}/>
-            })}
+            })} */}
 
         </div>
     )
