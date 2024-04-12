f9c06cb77345de1464bdf8511a52d6a6a7af1b1a
Baylee Lehmann <baylee.lehmann@gmail.com>
2024-04-12 14:58:01 -0700

    moved folders

--- week12/exercise/sounds.js
+++ week12/exercise/sounds.js
@@ -1,21 +1,14 @@
 let song; 
 let button;
 let amp;
-//let sound, amplitude, p5;
 
 function setup() {
     createCanvas(600, 600);
-    //cnv.mouseClicked(togglePlay);
-    //song = loadSound("bubbles_1.mp3", loaded); wrong
-   // amplitude = new p5.Amplitude();
+    song = loadSound("bubbles_1.mp3", loaded);
     amp = new p5.Amplitude();
     background(0);
 }
-// function preload() {
-//     sound= loadSound('bubbles_1.mp3');
-//     //console.log("loaded");
-//     button = createButton("play");
-// }
+
 
 function loaded() {
     console.log("loaded");
@@ -26,14 +19,12 @@ function loaded() {
 
 function draw () {
     background(255);
-    //text('tap to play', 20, 20);
-    //let level = amplitude.getLevel();
-    //let size = map(level, 0, 1, 0, 200);
-   // ellipse(width/2, height/2, size, size);
     let vol = amp.getLevel();
     let diam = map(vol, 0, 0.5, 50, 2000);
-    // fill(190, 216, 206);
-    // ellipse(width / 2, height / 2, diam, diam);
+    stroke(245, 107, 109);
+    strokeWeight(5);
+    fill(255, 0, 0);
+    ellipse(width / 2, height / 2, diam, diam);
 
 
 }
@@ -48,13 +39,3 @@ function draw () {
         button.html("play");
     }
     }
-
-// function togglePlay() {
-//     if (sound.isPlaying() ){
-//       sound.pause();
-//     } else {
-//       sound.loop();
-//           amplitude = new p5.Amplitude();
-//           amplitude.setInput(sound);
-//     }
-//   }
