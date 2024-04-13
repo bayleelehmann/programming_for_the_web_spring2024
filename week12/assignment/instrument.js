aeac51cf7e96095ae1a8ca9219dee0ebd3083352
Baylee Lehmann <baylee.lehmann@gmail.com>
2024-04-13 09:01:46 -0700

    assignment

--- week12/assignment/instrument.js
+++ week12/assignment/instrument.js
@@ -1,4 +1,32 @@
-function setup() {
-    createCanvas(600, 600);
-    background(54, 111, 170);
-}
+let sound1,
+    sound2,
+    sound3,
+    sound4;
+
+  
+//sounds
+window.addEventListener("DOMContentLoaded", function () {
+    sound1 = new Audio("sounds/sound1.wav");
+    sound2 = new Audio("sounds/sound2.wav");
+    sound3 = new Audio("sounds/sound3.wav");
+    sound4 = new Audio("sounds/sound4.wav");
+
+});
+
+//event listeners
+document.addEventListener("DOMContentLoaded", function (){
+    document.getElementById("sound1").addEventListener("mousedown", function () {
+        sound1.play();
+    });
+    document.getElementById("sound2").addEventListener("mousedown", function () {
+        sound2.play();
+    });
+    document.getElementById("sound3").addEventListener("mousedown", function () {
+        sound3.play();
+    });
+    document.getElementById("sound4").addEventListener("mousedown", function () {
+    sound4.play();
+    });
+
+});
+
