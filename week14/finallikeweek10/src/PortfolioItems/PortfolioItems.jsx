9808a69054611f8e82fce8398b553f436c195710
Baylee Lehmann <baylee.lehmann@gmail.com>
2024-04-28 19:48:39 -0700

    final

--- week14/finallikeweek10/src/PortfolioItems/PortfolioItems.jsx
+++ week14/finallikeweek10/src/PortfolioItems/PortfolioItems.jsx
@@ -1,4 +1,6 @@
 import lineBoilImg from "../assets/bayleelehmannlineboil_3.gif";
+// import "../contact/contact.html";
+// import "../about/about.html";
 import "./PortfolioItems.css";
 export default function PortfolioItems() {
     return (
@@ -6,6 +8,12 @@ export default function PortfolioItems() {
             <div className="lineBoilImg">
             <img src={lineBoilImg} alt="Baylee's items"/>
             </div>
+            <div className="topnav">
+             <a className="active" href="#home">Home</a>
+             <a href="../about/about.html">About</a>
+             <a href="../contact/contact.html">Contact</a>
+             <a href="https://www.linkedin.com/in/baylee-lehmann-194bb311/">LinkedIn</a>
+             </div>
         </div>
     )
 }
